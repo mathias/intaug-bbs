1250b8cb38f28b8aab302ce8d08b19f1079416d8
Matt Gauger <matt.gauger@gmail.com>
2017-03-23 18:33:37 -0500

    Add Mailgun settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,16 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.smtp_settings = {
+    address: ENV['MAILGUN_SMTP_SERVER'],
+    authentication: :plain,
+    domain: (ENV['MAILGUN_DOMAIN'] || 'intaug.org'),
+    password: ENV['MAILGUN_SMTP_PASSWORD'],
+    port: ENV['MAILGUN_SMTP_PORT'],
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+  }
+  config.action_mailer.delivery_method = :smtp
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
