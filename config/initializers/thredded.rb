8f51faf60d398c3a8c5c30ed336d7db579b1be5d
Matt Gauger <matt.gauger@gmail.com>
2017-03-23 19:41:38 -0500

    More mail settings

--- config/initializers/thredded.rb
+++ config/initializers/thredded.rb
@@ -64,10 +64,10 @@ Thredded.messageboards_order = :position
 
 # ==> Email Configuration
 # Email "From:" field will use the following
-# Thredded.email_from = 'no-reply@example.com'
+Thredded.email_from = 'no-reply@intaug.org'
 
 # Emails going out will prefix the "Subject:" with the following string
-# Thredded.email_outgoing_prefix = '[My Forum] '
+ Thredded.email_outgoing_prefix = '[My Forum] '
 
 # ==> View Configuration
 # Set the layout for rendering the thredded views.
