97309f23bb11d17d2ad86b9ae17f7ffe2a6a332a
Matt Gauger <matt.gauger@gmail.com>
2017-03-26 19:21:19 -0500

    Add about page with attribution for new logo

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
   mount Thredded::Engine => '/forum'
+  get '/about', to: 'home#about'
 end
