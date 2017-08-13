6f116f1648d70ab0426647c25eb50f204e47cc48
Matt Gauger <matt.gauger@gmail.com>
2017-08-13 11:10:24 -0500

    Fix newlines in routes file to my liking

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   root to: 'home#show'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
-  mount Thredded::Engine => '/forum'
-  get '/about', to: 'home#about'
 
+  mount Thredded::Engine => '/forum'
+
+  get '/about', to: 'home#about'
   get '/not-found', to: 'home#not_found'
   match '*anything', to: redirect('/not-found'), via: :all
 end
